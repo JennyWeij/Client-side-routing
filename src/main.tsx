28f02cc607d9fcff036b318148ce40ca85a74542
JennyWeij <jennyweij@outlook.com>
2023-02-25 16:36:07 +0100

    uppdaterat tab size och lagt till Header

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router';
+import { createRoutesFromElements, Route, RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
-  {
-    path: "pricing",
-    element: <div>Pricing</div>,
-  },
-]);
-
-// const routerJSX = createBrowserRouter(createRoutesFromElements(
-//   <Routes>
-//     <Route path="/" element= {<App />} />
-//     <Route path="about" element= {<div>About</div>} />
-//     <Route path="pricing" element= {<div>Pricing</div>} />
-//   </Routes>
-// ))
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route>
+    <Route path="/" element= {<App />} />
+    <Route path="about" element= {<div>About</div>} />
+    <Route path="pricing" element= {<div>Pricing</div>} />
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
